dd48d3d5ebb42e20cb2aba2283e19b6d4c188bc4
thientruong <tontruongdz@gmail.com>
2025-10-09 10:16:46 +0700

    Thêm DTO cho Báo cáo & Ví

---
+++ SCMS.Domain/DTOs/SalesSummaryDto.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace SCMS.Domain.DTOs
+{
+    public class SalesSummaryDto
+    {
+        public DateTime StartDate { get; set; }
+        public DateTime EndDate { get; set; }
+        public int TotalOrders { get; set; }
+        public decimal TotalRevenue { get; set; }
+        public List<TopSellingItem> TopItems { get; set; } = new List<TopSellingItem>();
+
+        public class TopSellingItem
+        {
+            public string ItemName { get; set; }
+            public int TotalQuantity { get; set; }
+        }
+    }
+}
