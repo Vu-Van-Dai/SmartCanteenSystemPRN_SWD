7e0ae0a770121c7f80fe5e11cc502026ad73bf47
Raichi <vudaittk31@gmail.com>
2025-10-15 07:34:00 +0700

    Thêm ApplicationDbContext

---
+++ SCMS.Infrastructure/ApplicationDbContextFactory.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+using Microsoft.Extensions.Configuration; // <--- Dòng using quan trọng được thêm vào
+using System.IO;
+
+namespace SCMS.Infrastructure
+{
+    public class ApplicationDbContextFactory : IDesignTimeDbContextFactory<ApplicationDbContext>
+    {
+        public ApplicationDbContext CreateDbContext(string[] args)
+        {
+            IConfigurationRoot configuration = new ConfigurationBuilder()
+                .SetBasePath(Path.Combine(Directory.GetCurrentDirectory(), "../SCMS.API"))
+                .AddJsonFile("appsettings.json")
+                .Build();
+
+            var builder = new DbContextOptionsBuilder<ApplicationDbContext>();
+
+            var connectionString = configuration.GetConnectionString("DefaultConnection");
+
+            builder.UseSqlServer(connectionString);
+
+            return new ApplicationDbContext(builder.Options);
+        }
+    }
+}
