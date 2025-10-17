713c4db65b52d4a9e317e07fc5b6a999994175f9
CND <nhatdungcao@gmail.com>
2025-10-17 20:22:41 +0700

    [Fix] MenuController binding & route

--- SCMS.API/Controllers/MenuController.cs
+++ SCMS.API/Controllers/MenuController.cs
@@ -44,6 +44,35 @@ namespace SCMS.API.Controllers
         }
 
 
-        
+        [HttpPost]
+        [Authorize(Roles = "CanteenManager,SystemAdmin")]
+        public async Task<IActionResult> Create([FromForm] CreateMenuItemDto menuItemDto, IFormFile? imageFile)
+        {
+            var createdItem = await _menuService.CreateMenuItemAsync(menuItemDto, imageFile);
+
+            if (createdItem == null)
+            {
+                return BadRequest("Invalid CategoryId or error processing image.");
+            }
+            return CreatedAtAction(nameof(GetById), new { id = createdItem.ItemId }, createdItem);
+        }
+
+        [HttpPut("{id}")]
+        [Authorize(Roles = "CanteenManager,SystemAdmin")]
+        public async Task<IActionResult> Update(int id, [FromForm] UpdateMenuItemDto menuItemDto, IFormFile? imageFile)
+        {
+            var success = await _menuService.UpdateMenuItemAsync(id, menuItemDto, imageFile);
+            if (!success) return NotFound();
+            return NoContent();
+        }
+
+        [HttpDelete("{id}")]
+        [Authorize(Roles = "CanteenManager,SystemAdmin")]
+        public async Task<IActionResult> Delete(int id)
+        {
+            var success = await _menuService.DeleteMenuItemAsync(id);
+            if (!success) return NotFound();
+            return NoContent();
+        }
     }
 }
