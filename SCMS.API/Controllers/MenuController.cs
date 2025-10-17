e3cf8d0a6c39a0733c80b444c835c33da58f1c51
CND <nhatdungcao@gmail.com>
2025-10-17 13:17:25 +0700

    Add MenuController

---
+++ SCMS.API/Controllers/MenuController.cs
@@ -0,0 +1,49 @@
+﻿// File: SCMS.API/Controllers/MenuController.cs
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http; // THÊM using này
+using Microsoft.AspNetCore.Mvc;
+using SCMS.Application;
+using SCMS.Domain.DTOs;
+using System.Threading.Tasks;
+
+namespace SCMS.API.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class MenuController : ControllerBase
+    {
+        private readonly IMenuService _menuService;
+
+        public MenuController(IMenuService menuService)
+        {
+            _menuService = menuService;
+        }
+
+        [HttpGet]
+        [AllowAnonymous]
+        public async Task<IActionResult> Get([FromQuery] string? searchTerm, [FromQuery] int? categoryId)
+        {
+            var menuItems = await _menuService.GetMenuItemsAsync(searchTerm, categoryId);
+            return Ok(menuItems);
+        }
+
+        [HttpGet("categories")]
+        [AllowAnonymous]
+        public async Task<IActionResult> GetCategories()
+        {
+            var categories = await _menuService.GetAllCategoriesAsync();
+            return Ok(categories);
+        }
+
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GetById(int id)
+        {
+            var menuItem = await _menuService.GetMenuItemByIdAsync(id);
+            if (menuItem == null) return NotFound();
+            return Ok(menuItem);
+        }
+
+
+        
+    }
+}
