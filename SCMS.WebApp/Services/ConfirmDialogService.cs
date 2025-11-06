aeafa1cf47847c53f3e9024941cbc330bd8e3101
PhatMessi <nguyenluongphat1609@gmail.com>
2025-11-06 10:28:58 +0700

    Add ConfirmDialogService, ToastService to do Confirm FE.

---
+++ SCMS.WebApp/Services/ConfirmDialogService.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace SCMS.WebApp.Services
+{
+    public class ConfirmDialogService
+    {
+        public event Func<string, string, Task<bool>>? OnShow;
+
+        public async Task<bool> Show(string title, string message)
+        {
+            if (OnShow != null)
+            {
+                return await OnShow.Invoke(title, message);
+            }
+            return false;
+        }
+    }
+}
